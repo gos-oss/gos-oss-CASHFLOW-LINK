56469f7f9ceaa72c1ed83cf50787fcc440a6f270
gos-oss <gos@linkinversiones.com.ar>
2026-08-11 15:35:23 -0300

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,7 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
-// Reemplaza estos valores con las credenciales de tu panel de Supabase
-const supabaseUrl = 'TU_SUPABASE_URL_AQUI';
-const supabaseAnonKey = 'TU_SUPABASE_ANON_KEY_AQUI';
+// Credenciales de tu proyecto en Supabase
+const supabaseUrl = 'https://kbmbdvcawbhumlyjxkam.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtibWJkdmNhd2JodW1seWp4a2FtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODYzNzM4ODYsImV4cCI6MjEwMTk0OTg4Nn0.b75eLNTYpyby27jBh6IdblZo1RiUi4_zdAKMT-b6hZY';
 
+// Inicialización del cliente oficial
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
